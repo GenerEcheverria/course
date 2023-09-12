387b4bdfe23d9e267b5786a53f40f8f49122d365
GenerEcheverria <generalex@live.com.mx>
2023-09-12 12:32:50 -0600

    Tarea POO TDD

--- inclass/src/3_TDD/objectOriented/BowlingGame.spec.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.spec.ts
@@ -8,4 +8,52 @@ describe('Test Bowling Game Functionality', () => {
     game = new BowlingGame()
   });
 
+  it('can roll', () => {
+    expect(game.roll(0)).toBeDefined;
+  });
+
+  it('should calculate score of gutterGame', () => {
+    rollMany(game, 20, 0);
+    expect(game.score()).toBe(0);
+  });
+
+  it('should calculate score with all 1', () => {
+    rollMany(game, 20, 1);
+    expect(game.score()).toBe(20);
+  });
+
+  it('can roll a spare', () => {
+    rollSpare(game);
+    game.roll(3);
+    rollMany(game,17,0)
+    expect(game.score()).toBe(16);
+  });
+
+  it('can roll a strike', () => {
+    rollStrike(game);
+    game.roll(2);
+    game.roll(2);
+    rollMany(game, 16, 0);
+    expect(game.score()).toBe(18);
+  });
+
+  it('can roll a perfect game', () => {
+    rollMany(game, 12, 10);
+    expect(game.score()).toBe(300);
+  });
+  
+  function rollMany(game: BowlingGame, rolls: number, pins: number) {
+    for (let i = 0; i < rolls; i++) {
+      game.roll(pins);
+    }
+  }
+
+  function rollSpare(game: BowlingGame) {
+    game.roll(5);
+    game.roll(5);
+  }
+
+  function rollStrike(game: BowlingGame) {
+    game.roll(10);
+  }
 });
--- inclass/src/3_TDD/objectOriented/BowlingGame.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.ts
@@ -1,6 +1,53 @@
-
 export class BowlingGame {
+  private pins: number[]
+  private MAX_PINS: number
+  
+  constructor() {
+    this.pins = []
+    this.MAX_PINS = 10;
+   }
 
-  constructor() { }
+  public roll(pins: number){
+    return this.pins.push(pins)
+  }
+
+  public score() {
+    let score = 0;
+    let firstRoll = 0;
+    const FRAMES = 10;
+    for(let i = 0; i < FRAMES; i++){
+      if (this.isSpare(firstRoll)) {
+        score += this.spareScore(firstRoll);
+        firstRoll += 2;
+      } else if (this.isStrike(firstRoll)) {
+        score += this.strikeScore(firstRoll);
+        firstRoll += 1;
+      } else {
+        score += this.regularFrameScore(firstRoll);
+        firstRoll += 2;
+      }
+    }
+    return score
+  }
+
+  private regularFrameScore(firstRoll: number): number {
+    return this.pins[firstRoll] + this.pins[firstRoll + 1]
+  }
+
+  private isSpare(firstRoll: number): boolean {
+    return (this.pins[firstRoll] + this.pins[firstRoll + 1]) === this.MAX_PINS
+  }
+
+  private spareScore(firstRoll: number): number {
+    return this.MAX_PINS + this.pins[firstRoll+2];
+  }  
+
+  private isStrike(firstRoll: number): boolean {
+    return this.pins[firstRoll] === this.MAX_PINS 
+  }
+
+  private strikeScore(firstRoll: number): number {
+    return this.MAX_PINS + this.pins[firstRoll + 1] + this.pins[firstRoll + 2];
+  }
 
 }
